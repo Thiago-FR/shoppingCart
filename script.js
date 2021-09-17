e0d69af2681a8ec6d1ff6b58c9a7d19fad690c74
Thiago <thiago_ssfr@hotmail.com>
2021-09-17 16:22:35 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// const fetch = require('node-fetch');
+
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,7 +19,6 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -24,20 +27,44 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+const createItem = (obj) => {
+  obj.forEach((element) => {
+    const sectionItem = document.querySelector('.items');
+    sectionItem.appendChild(createProductItemElement(element));
+  });
+};
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  };
+
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    // .then((data) => console.log(data.results));
+    .then((data) => data.results.map(({ id, title, thumbnail }) => ({
+      sku: id,
+      name: title,
+      image: thumbnail,
+     })))
+     .then((data) => createItem(data));
+};
+
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} 
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */ 
 
-window.onload = () => { };
+window.onload = () => fetchJoke();
