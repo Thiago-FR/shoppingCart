17d8d5b6141dbbaeed61210fed12cc7793d51c23
Thiago <thiago_ssfr@hotmail.com>
2021-09-17 22:46:01 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const API_URL_CATEGORIA = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 const API_URL_ITEM = 'https://api.mercadolibre.com/items/';
 
+const totalPriceClass = '.total-price';
+const classCartItem = '.cart__items';
+
 const myObject = {
   method: 'GET',
   headers: { Accept: 'application/json' },
@@ -40,43 +43,67 @@ const createItem = (obj) => {
   });
 };
 
+const loadPrice = () => {
+  const priceAtual = document.querySelector(totalPriceClass);
+  if (localStorage.length > 0) {
+   priceAtual.innerHTML = localStorage.getItem(localStorage.length);
+  }
+};
+
 const removeSaveAll = () => {
   localStorage.clear();
 };
 
 const saveShop = () => {
   const local = document.querySelectorAll('.cart__item');
+  const priceAtual = document.querySelector(totalPriceClass);
   removeSaveAll();
   for (let index = 0; index < local.length; index += 1) {
     const data = {
       class: local[index].className,
-      text: local[index].innerText,
+      text: local[index].innerText,      
     };
     localStorage.setItem(index, JSON.stringify(data));
   }
+  localStorage.setItem(localStorage.length + 1, priceAtual.innerHTML);
+  if (localStorage.length === 1) removeSaveAll();
 };
 
 /* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 } */
 
+const sumPrice = (price, operador) => {
+  const priceAtual = document.querySelector(totalPriceClass);
+  let sum = 0;
+  if (operador === '+') {
+    sum = parseFloat(priceAtual.innerHTML, 10) + price;
+  } else {
+    sum = parseFloat(priceAtual.innerHTML, 10) - price;
+  }
+  priceAtual.innerHTML = sum;
+  console.log(sum);
+};
+
 function cartItemClickListener(event) {
   const delet = event.target;
+  const removePrice = parseFloat(delet.innerText.split('$')[1]);
+  sumPrice(removePrice, '-');
   delet.remove();
   saveShop();
 }
 
 const loadShop = () => {  
-  const sectionItemCart = document.querySelector('.cart__items');  
-  for (let index = 0; index < localStorage.length; index += 1) {
+  const sectionItemCart = document.querySelector(classCartItem);  
+  for (let index = 0; index < localStorage.length - 1; index += 1) {
     const data = JSON.parse(localStorage.getItem(index));
     const li = document.createElement('li');
     li.className = data.class;
     li.innerText = data.text;
     li.addEventListener('click', cartItemClickListener);
     sectionItemCart.appendChild(li);
-    console.log(data);
   }
+  loadPrice();
 };
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -88,13 +115,15 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 const createItemCart = (obj) => {  
-  const sectionItemCart = document.querySelector('.cart__items');
+  const sectionItemCart = document.querySelector(classCartItem);
   sectionItemCart.appendChild(createCartItemElement(obj));
+  const { salePrice } = obj;
+  sumPrice(salePrice, '+');
   saveShop();
 };
 
 const fetItem = (url) => {
-  fetch(url, myObject)
+ fetch(url, myObject)
   .then((response) => response.json())
   .then(({ id, title, price }) => ({
     sku: id,
@@ -106,9 +135,8 @@ const fetItem = (url) => {
 
 const btnCart = () => {
   const btnAddCart = document.querySelectorAll('.item__add');
-
   btnAddCart.forEach((btn) => {
-    btn.addEventListener('click', (event) => {
+    btn.addEventListener('click', async (event) => {
       const idItem = event.target.parentNode.firstChild.innerText;
       const url = `${API_URL_ITEM}${idItem}`;
       fetItem(url);
@@ -116,10 +144,22 @@ const btnCart = () => {
   });
 };
 
+const btnEmpty = () => {
+  const btn = document.querySelector('.empty-cart');
+  const priceAtual = document.querySelector(totalPriceClass);
+  btn.addEventListener('click', () => {
+    const sectionItemCart = document.querySelector('.cart__items');
+    while (sectionItemCart.firstChild) {
+      sectionItemCart.firstChild.remove();
+    }
+    priceAtual.innerHTML = '0';
+    removeSaveAll();
+  });
+};
+
 const fetCategoria = (url) => {
   fetch(url, myObject)
   .then((response) => response.json())
-  // .then((data) => console.log(data.results));
   .then((data) => data.results.map(({ id, title, thumbnail }) => ({
     sku: id,
     name: title,
@@ -132,4 +172,5 @@ const fetCategoria = (url) => {
 window.onload = () => {  
   fetCategoria(API_URL_CATEGORIA);
   loadShop();
+  btnEmpty();
 };
